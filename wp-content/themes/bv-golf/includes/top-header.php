7b34049c4aec80f901ea6ba27607c332223dba19
KelvinoAlbino <kelvinyoung@Kelvins-MacBook-Pro.local>
2017-04-02 14:27:28 +0200

    Add top header details

---
+++ wp-content/themes/bv-golf/includes/top-header.php
@@ -0,0 +1,12 @@
+<div id="top-header">
+  <div class="container">
+    <ul>
+      <li><a href="/">011 123 1234</a></li>
+      <li><a href="/">info@bv-golf.com</a></li>
+    </ul>
+    <ul class="logins">
+      <li><a href="<?php echo site_url(); ?>/membership-login/">Sign in</a></li>
+      <li><a href="<?php echo site_url(); ?>/membership-join/">Join us</a></li>
+    </ul>
+  </div>
+</div>
