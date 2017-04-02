7e372407aa5c9db7135423508209c2595d2a26f5
KelvinoAlbino <kelvinyoung@Kelvins-MacBook-Pro.local>
2017-04-02 15:28:47 +0200

    add font awesome

--- wp-content/themes/bv-golf/includes/top-header.php
+++ wp-content/themes/bv-golf/includes/top-header.php
@@ -1,12 +1,20 @@
 <div id="top-header">
   <div class="container">
     <ul>
-      <li><a href="/">011 123 1234</a></li>
-      <li><a href="/">info@bv-golf.com</a></li>
+      <li>
+        <a href="tel:+27111231234"><i class="fa fa-phone" aria-hidden="true"></i> 011 123 1234</a>
+      </li>
+      <li>
+        <a href="mailto:info@bv-golf.com"><i class="fa fa-envelope" aria-hidden="true"></i> info@bv-golf.com</a>
+      </li>
     </ul>
     <ul class="logins">
-      <li><a href="<?php echo site_url(); ?>/membership-login/">Sign in</a></li>
-      <li><a href="<?php echo site_url(); ?>/membership-join/">Join us</a></li>
+      <li>
+        <a href="<?php echo site_url(); ?>/membership-login/"><i class="fa fa-lock" aria-hidden="true"></i> Sign in</a>
+      </li>
+      <li>
+        <a href="<?php echo site_url(); ?>/membership-join/"><i class="fa fa-users" aria-hidden="true"></i> Join us</a>
+      </li>
     </ul>
   </div>
 </div>
